80207587fa4a263c795d39f83958c76a20796f3f
Crisila Aban <Crisila@MacBook-Pro-Alisirc.local>
2023-04-16 10:59:03 -0700

    complete

---
+++ src/components/ColorBlock.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function ColorBlock({ color }) {
+    let style = {'backgroundColor': color}
+    return <div className="colorBlock"
+                style={style}
+    >
+            <p>{color}</p>
+        </div>
+
+}
